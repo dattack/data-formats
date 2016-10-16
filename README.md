297bd6a783963f679f822215ad779a1e65985fc2
cvarela <cvarela@dattack.com>
2016-10-16 19:48:35 +0200

    Initial commit. CSV support

---
+++ README.md
@@ -0,0 +1,25 @@
+[![Travis Badge](https://secure.travis-ci.org/dattack/data-formats.svg?branch=master)](https://travis-ci.org/dattack/data-formats/builds)
+[![license](https://img.shields.io/:license-Apache-blue.svg?style=plastic-square)](LICENSE.md)
+
+data-formats
+=========
+
+A very simple Java library for reading and writing data in different formats.
+Currently, the available formats are:
+
+* CVS (Comma Separated Values).
+
+Copyright and license
+=========
+
+[![][license img]][license]
+
+Copyright 2016 Dattack Team
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this work except in compliance with the License. You may obtain a copy of the License in the LICENSE file, or at:
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+
+This product includes software developed by The Apache Software Foundation (http://www.apache.org/).
---
+++ src/main/java/com/dattack/formats/csv/CSVReader.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2016, The Dattack team (http://www.dattack.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.dattack.formats.csv;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * @author cvarela
+ * @since 0.1
+ */
+public class CSVReader implements Closeable {
+
+    private final CSVConfiguration configuration;
+    private final File dataFile;
+    private BufferedReader bufferedReader;
+
+    public CSVReader(final CSVConfiguration configuration, final File dataFile) {
+        this.configuration = configuration;
+        this.dataFile = dataFile;
+    }
+
+    @Override
+    public void close() throws IOException {
+        if (bufferedReader != null) {
+            bufferedReader.close();
+        }
+    }
+
+    private BufferedReader getReader() throws IOException {
+
+        if (bufferedReader == null) {
+            bufferedReader = new BufferedReader(new FileReader(dataFile));
+        }
+        return bufferedReader;
+    }
+
+    private boolean isComment(final String line) {
+        return line.charAt(0) == configuration.getCommentChar();
+    }
+
+    /**
+     * Iterate over the reader and returns the next object.
+     *
+     * @return the next object o <tt>null</tt> if no one exists
+     * @throws IOException
+     *             if an I/O error occurs
+     */
+    public synchronized CSVObject next() throws IOException {
+
+        String line = "";
+        CSVObject object = null;
+        while (object == null && (line = getReader().readLine()) != null) {
+
+            line = line.trim();
+            if (line.isEmpty() || isComment(line)) {
+                continue;
+            }
+            final String[] data = line.split(configuration.getSeparator());
+            object = new CSVObject(data);
+        }
+
+        return object;
+    }
+}
