f914f7217f3259f298ed6530ac56914c1a0cc2f6
Carlos Varela <cvarela@dattack.com>
2021-05-01 12:15:10 +0200

    Update Travis-CI integration

--- .travis/before-deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-openssl aes-256-cbc -K $encrypted_a8da13599b2d_key -iv $encrypted_a8da13599b2d_iv -in .travis/codesigning.asc.enc -out .travis/codesigning.asc -d
-gpg --fast-import .travis/codesigning.asc
